87b90fdb38cf0c0be1b12fd50b3f0d0def349136
xiaojueshi <dev@xiaojueshi.top>
2025-02-27 13:52:46 +0800

    feat(components): 优化社交媒体链接并添加 Font Awesome 支持
    - 在 BaseHead 组件中添加 Font Awesome CDN 链接
    - 重构 Footer 组件中的社交媒体链接，使用配置文件动态生成
    - 新增 config.ts 文件，集中管理网站配置信息

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+export const SITE_TITLE = 'XIAOJUESHI';
+export const SITE_DESCRIPTION = '分享前端开发经验、技术见解和编程思考的个人博客';
+
+export const SOCIAL_MEDIA = [
+  {
+    name: 'GitHub',
+    url: 'https://github.com/xiaojueshi',
+    icon: 'fab fa-github',
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/astrodotbuild',
+    icon: 'fab fa-twitter',
+  },
+];
